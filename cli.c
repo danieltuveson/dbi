d014c4f31802a83e9109c8c6dfb36c9f9418d51c
danieltuveson <danieltuveson@gmail.com>
2026-02-16 20:53:20 -0500

    help flag

--- cli.c
+++ cli.c
@@ -8,6 +8,20 @@
 // Could just as easily replace this with a `!`, but that gets a little confusing
 #define status(val) val ? EXIT_SUCCESS : EXIT_FAILURE
 
+void print_cli_help(void)
+{
+    printf("Usage: dbi [options] \n"
+            "Options:\n"
+            "  -c file    compile file and print resulting bytecode\n"
+            "  -e file    execute file\n"
+            // "  -r file    execute file and start repl\n"
+          );
+}
+
+const char *bad_input =
+"Error: unknown argument %s\n"
+"Run `dbi -h` for s list of valid arguments\n";
+
 int main(int argc, char *argv[])
 {
     DbiProgram prog = dbi_program_new();
@@ -17,9 +31,15 @@ int main(int argc, char *argv[])
     if (argc < 2) {
         ret = status(dbi_repl(prog, NULL));
     } else if (argc == 2) {
-        ret = status(dbi_repl(prog, argv[1]));
-        if (ret == EXIT_FAILURE) {
-            printf("%s", dbi_strerror());
+        if (strcmp(argv[1], "-h") == 0) {
+            print_cli_help();
+        } else if (argv[1][0] != '-') {
+            ret = status(dbi_repl(prog, argv[1]));
+            if (ret == EXIT_FAILURE) {
+                printf("%s", dbi_strerror());
+            }
+        } else {
+            printf(bad_input, argv[1]);
         }
     } else if (argc == 3) {
         if (strcmp(argv[1], "-c") == 0) {
@@ -41,10 +61,10 @@ int main(int argc, char *argv[])
                 }
                 dbi_runtime_free(dbi);
             }
-        } else if (strcmp(argv[1], "-r") == 0) {
-            ret = status(dbi_repl(prog, argv[2]));
+        // } else if (strcmp(argv[1], "-r") == 0) {
+        //     ret = status(dbi_repl(prog, argv[2]));
         } else {
-            printf("Error: unknown argument %s\n", argv[1]);
+            printf(bad_input, argv[1]);
         }
     } else {
         printf("Error: invalid arguments\n");
